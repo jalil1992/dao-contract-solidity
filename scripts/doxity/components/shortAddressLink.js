8209cf6d053c1a8f5c82af2109d74b09b3dd0878
JM <jalilmarkel1992@gmail.com>
2020-10-06 07:45:17 +0800

    Fixed some issues at  shortAddressLink

--- scripts/doxity/components/shortAddressLink.js
+++ scripts/doxity/components/shortAddressLink.js
@@ -1,19 +1,19 @@
-import React, { PropTypes, Component } from 'react'
+
 import { config } from 'config'
 
-export default class ShortAddressLink extends Component {
+    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+
   render() {
-    const { interaction } = config
-    if (!interaction) { return null }
-    const { address } = this.props
+  render() {
+
+    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+  render() {
+    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
+    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
+
+
+    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
     const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
     const url = `https://${urlPrefix}etherscan.io/address/${address}`
-    return (
-      <a target="_blank" href={url}>{address}</a>
-    )
-  }
-}
-
-ShortAddressLink.propTypes = {
-  address: PropTypes.string.isRequired,
-}
