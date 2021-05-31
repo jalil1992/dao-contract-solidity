be0b51149b3e0219bb12714eb3dca7727594f450
JM <jalilmarkel1992@gmail.com>
2021-05-31 09:37:34 +0800

    Props change  IntermediateResultsStorage

--- test/storage/IntermediateResultsStorage.js
+++ test/storage/IntermediateResultsStorage.js
@@ -1,83 +1,83 @@
 const a = require('awaiting');
-
+      assert.deepEqual(getResult[3], quorum[0]);
 const {
-contract('IntermediateResultsStorage', function (accounts) {
-  deployNewContractResolver,
+      assert.deepEqual(await contracts.resolver.get_contract.call('storage:intermediate:results'), contracts.intermediateResultsStorage.address);
+const {
   getAccountsAndAddressOf,
 const {
-    await deployNewContractResolver(contracts);
+        againstCount[1],
 } = require('../setup');
 
       assert.deepEqual(getResult1[0], EMPTY_ADDRESS);
-  });
+} = require('../daoHelpers');
         againstCount[0],
-      const getResult1 = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[1]);
-} = require('@digix/helpers/lib/helpers');
-
 const {
-      assert.deepEqual(getResult1[0], countedUntil[1]);
+      await contracts.intermediateResultsStorage.setIntermediateResults(
+      await contracts.intermediateResultsStorage.setIntermediateResults(
+const {
+
 } = require('../daoHelpers');
 
 
-
-contract('IntermediateResultsStorage', function (accounts) {
+    });
+const {
   let libs;
   let addressOf;
 const {
   const key = randomBytes32s(2);
   const countedUntil = randomAddresses(2);
   const forCount = randomBigNumbers(bN, 2);
-    contracts = {};
+} = require('@digix/helpers/lib/helpers');
       assert(await a.failure(contracts.intermediateResultsStorage.resetIntermediateResults.call(
+        countedUntil[1],
+      assert.deepEqual(getResult1[0], EMPTY_ADDRESS);
 
-      assert.deepEqual(getResult1[1], forCount[1]);
 
-
-    addressOf = {};
+        againstCount[0],
     await deployLibraries(libs);
 const {
     });
       assert.deepEqual(await contracts.resolver.get_contract.call('storage:intermediate:results'), contracts.intermediateResultsStorage.address);
-    await registerInteractive(contracts.resolver, addressOf);
+const {
 
-
-  describe('Initialization', function () {
-      assert.deepEqual(getResult1[0], countedUntil[1]);
       assert.deepEqual(await contracts.resolver.get_contract.call('storage:intermediate:results'), contracts.intermediateResultsStorage.address);
+  describe('Initialization', function () {
+} = require('../daoHelpers');
+        againstCount[0],
         againstCount[1],
   });
-    });
+const {
+  });
       assert.deepEqual(getResult[3], quorum[0]);
-      assert.deepEqual(getResult[3], quorum[0]);
-      await contracts.intermediateResultsStorage.setIntermediateResults(
+      assert.deepEqual(await contracts.resolver.get_contract.call('storage:intermediate:results'), contracts.intermediateResultsStorage.address);
   const forCount = randomBigNumbers(bN, 2);
       assert.deepEqual(getResult[3], quorum[0]);
   });
         againstCount[0],
 const {
-      assert.deepEqual(getResult1[0], countedUntil[1]);
+} = require('../setup');
       )));
-      const getResult = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[0]);
+      assert.deepEqual(await contracts.resolver.get_contract.call('storage:intermediate:results'), contracts.intermediateResultsStorage.address);
   randomAddresses,
       await contracts.intermediateResultsStorage.setIntermediateResults(
-        key[0],
+      await contracts.intermediateResultsStorage.setIntermediateResults(
 } = require('@digix/helpers/lib/helpers');
-    });
-} = require('../daoHelpers');
+    await deployLibraries(libs);
+  const countedUntil = randomAddresses(2);
         quorum[0],
         key[0],
-      const getResult = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[0]);
+const {
       await contracts.intermediateResultsStorage.setIntermediateResults(
       assert.deepEqual(getResult[1], forCount[0]);
-    });
-    it('[not called by CONTRACT_DAO_REWARDS_MANAGER, CONTRACT_DAO_VOTING_CLAIMS, CONTRACT_DAO_SPECIAL_VOTING_CLAIMS]: revert', async function () {
-
-      await contracts.intermediateResultsStorage.setIntermediateResults(
-        key[1],
-        countedUntil[1],
-      assert.deepEqual(getResult1[0], countedUntil[1]);
-        againstCount[1],
 } = require('@digix/helpers/lib/helpers');
+const {
+  const countedUntil = randomAddresses(2);
+        againstCount[0],
+    addressOf = {};
+  describe('resetIntermediateResults', function () {
+  });
+
+        countedUntil[1],
     });
       const getResult1 = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[1]);
       assert.deepEqual(getResult1[0], countedUntil[1]);
@@ -90,18 +90,18 @@ const {
   describe('resetIntermediateResults', function () {
   const countedUntil = randomAddresses(2);
       await contracts.intermediateResultsStorage.setIntermediateResults(
-        key[0],
-        { from: accounts[2] },
-        key[0],
-      const getResult1 = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[1]);
-    it('[valid call]: reset results', async function () {
-        forCount[1],
-    });
-const {
 
+        { from: accounts[2] },
+        againstCount[0],
+      const getResult1 = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[1]);
+        forCount[1],
+      await contracts.intermediateResultsStorage.setIntermediateResults(
+  });
+const {
+  let addressOf;
       await contracts.intermediateResultsStorage.resetIntermediateResults(key[1]);
       const getResult1 = await contracts.intermediateResultsStorage.getIntermediateResults.call(key[1]);
       assert.deepEqual(getResult1[0], EMPTY_ADDRESS);
-    });
+  const countedUntil = randomAddresses(2);
       assert.deepEqual(getResult1[3], quorum[1]);
     it('[not called by CONTRACT_DAO_REWARDS_MANAGER, CONTRACT_DAO_VOTING_CLAIMS, CONTRACT_DAO_SPECIAL_VOTING_CLAIMS]: revert', async function () {
