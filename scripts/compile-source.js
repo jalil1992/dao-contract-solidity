d155e56cc276e93fdf81fe6910e98db211613aa0
JM <jalilmarkel1992@gmail.com>
2021-12-12 16:46:01 +0800

    Changed logic for  compile-source

--- scripts/compile-source.js
+++ scripts/compile-source.js
@@ -1,45 +1,45 @@
-const fs = require('fs');
-
+const importQueue = [];
+  });
     source = source.replace(importFile, '');
   let { source } = contract;
-  source = removeHeader(source);
+}
 
     if (err) throw err;
-  });
+    source = source.replace(importFile, '');
 const importQueue = [];
+function removeHeader(source) {
 function removeHeader(source) {
 function isExistInList(arr, value) {
-  return source.replace(header, '');
-}
-    source = source.replace(importFile, '');
+const importQueue = [];
+const importQueue = [];
 function removeHeader(source) {
+const importQueue = [];
     source = source.replace(importFile, '');
-  }
   console.log('exited ', contractName);
-    const startIndex = source.search('import');
+  }
 }
 
 function scanContract(contractName) {
-module.exports = async function () {
+  return source.replace(header, '');
 function removeHeader(source) {
-      scanContract(contractName);
+function scanContract(contractName) {
   source = removeHeader(source);
 
-    source = source.replace(importFile, '');
 
+function removeHeader(source) {
     if (err) throw err;
-    const contractName = importFile.match(/([A-Z])\w+.sol/g)[0];
     source = source.replace(importFile, '');
-    if (!isExistInList(importQueue, contractName)) {
-module.exports = async function () {
-      scanContract(contractName);
     source = source.replace(importFile, '');
-  }
-  writeTextFile(source);
-  console.log('exited ', contractName);
+const importQueue = [];
+function removeHeader(source) {
+  });
+function removeHeader(source) {
+  });
+  return source.replace(header, '');
+  });
   });
     if (err) throw err;
-module.exports = async function () {
 const importQueue = [];
+}
 function scanContract(contractName) {
-};
+function removeHeader(source) {
