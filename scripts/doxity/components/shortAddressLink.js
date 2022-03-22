ccc19c79df2847457940152ef38db6167673f0a5
JM <jalilmarkel1992@gmail.com>
2022-03-22 20:37:05 +0800

    Fixed some issues at  shortAddressLink

--- scripts/doxity/components/shortAddressLink.js
+++ scripts/doxity/components/shortAddressLink.js
@@ -1,19 +1,18 @@
-
-import { config } from 'config'
-
-    const url = `https://${urlPrefix}etherscan.io/address/${address}`
-
+    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
   render() {
-  render() {
-
-    const url = `https://${urlPrefix}etherscan.io/address/${address}`
-    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
   render() {
     const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
     const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
-
-
-    const url = `https://${urlPrefix}etherscan.io/address/${address}`
-    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
     const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
     const url = `https://${urlPrefix}etherscan.io/address/${address}`
+  render() {
+  render() {
+  render() {
+    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+    const urlPrefix = interaction.network === '2' ? 'testnet.' : ''
+    const url = `https://${urlPrefix}etherscan.io/address/${address}`
+  render() {
+  render() {
+  render() {
+
